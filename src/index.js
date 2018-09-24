be1c374e39fcc09e4242cb1e20a13a4ee548e379
Nik <minscky.d123@yandex.ru>
2018-09-24 23:55:26 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function warmup(temperature) {
-  // your implementation here
+  
+  return temperature*9/5+32; //Чтобы перевести температуру из шкалы Цельсия в шкалу Фаренгейта, необходимо умножить исходное число на 9/5 и прибавить 32
+
+ //Подробнее: https://www.kakprosto.ru/kak-50267-kak-perevesti-gradusy-celsiya-v-farengeyt#ixzz5S3S9ofN7
+  
 };
